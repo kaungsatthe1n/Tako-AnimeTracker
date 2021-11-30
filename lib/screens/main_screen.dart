344d1b7e12ef80c72761f645fb7b0fa09943408a
kaungsatthe1n <parrotksh@gmail.com>
2021-11-30 15:24:39 +0630

    Connectivity Dependency Injected

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tako/provider/connectivitymanager.dart';
 import 'package:tako/provider/tabmanager.dart';
 import 'package:tako/screens/genre_categories_screen.dart';
+import 'package:tako/screens/no_internet_screen.dart';
 import 'package:tako/screens/searched_result_screen.dart';
 import 'package:tako/screens/home_screen.dart';
 
@@ -20,44 +22,57 @@ class _MainScreenState extends State<MainScreen> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    Provider.of<ConnectivityManager>(context, listen: false).startMornitoring();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Consumer<TabManager>(
-      builder: (context, tabManager, _) => Scaffold(
-          resizeToAvoidBottomInset: false,
-          appBar: AppBar(
-            actions: [
-              IconButton(
-                  onPressed: () => Navigator.of(context).push(MaterialPageRoute(
-                      builder: (_) => const SearchResultScreen())),
-                  icon: const Icon(Icons.search))
-            ],
-            centerTitle: true,
-            title: const Text(
-              'Tako Anime Tracker',
-            ),
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            onTap: tabManager.goToTab,
-            currentIndex: tabManager.selectedIndex,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home,
+        builder: (context, tabManager, _) => Scaffold(
+              resizeToAvoidBottomInset: false,
+              appBar: AppBar(
+                actions: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (_) => const SearchResultScreen()));
+                      },
+                      icon: const Icon(Icons.search)),
+                ],
+                centerTitle: true,
+                title: const Text(
+                  'Tako Anime Tracker',
                 ),
-                label: 'Home',
               ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.list,
-                ),
-                label: 'Genres',
+              bottomNavigationBar: BottomNavigationBar(
+                onTap: tabManager.goToTab,
+                currentIndex: tabManager.selectedIndex,
+                items: const [
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.home,
+                    ),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.list,
+                    ),
+                    label: 'Genres',
+                  ),
+                ],
               ),
-            ],
-          ),
-          body: IndexedStack(
-            children: _pages,
-            index: tabManager.selectedIndex,
-          )),
-    );
+              body: Consumer<ConnectivityManager>(
+                  builder: (context, connectivityManager, child) {
+                return connectivityManager.isOnline == true
+                    ? IndexedStack(
+                        children: _pages,
+                        index: tabManager.selectedIndex,
+                      )
+                    : const NoInternetScreen();
+              }),
+            ));
   }
 }
